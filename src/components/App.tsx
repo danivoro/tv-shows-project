a10b24d23dc96fb7e47d36ca830563427f53eb95
dani voro <mr.voro@gmail.com>
2023-07-26 19:41:06 +0200

    re-rendering fixed on fetching

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,33 +4,11 @@ import "./App.css";
 import Footer from "./Footer";
 import filterBySearchedInput from "../util/filterBySearchedInput";
 import IEpisode from "../interfaces/episode";
-// const fetchedEpisodes: IEpisode[] = _episodes as IEpisode[];
-let fetchedEpisodes: IEpisode[] = [];
-
-async function getDataFromAPI() {
-    const response = await fetch("https://api.tvmaze.com/shows/527/episodes");
-    const jsonBody = await response.json();
-    fetchedEpisodes = jsonBody;
-}
-
-getDataFromAPI();
 
 interface KeyboardControlledInputProps {
     value: string;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
-
-fetchedEpisodes.forEach((episodeInfo) => {
-    if (!episodeInfo.rating.average) {
-        episodeInfo.rating.average = 0;
-    }
-    if (!episodeInfo.summary) {
-        episodeInfo.summary = "";
-    }
-    if (!episodeInfo.image) {
-        episodeInfo.image = { medium: "", original: "" };
-    }
-});
 
 function KeyboardControlledInput(
     props: KeyboardControlledInputProps
@@ -44,19 +22,31 @@ function KeyboardControlledInput(
 
 function App() {
     const [searchedInput, setSearchedInput] = useState("");
+    const [fetchedEpisodes, setFetchedEpisodes] = useState<IEpisode[]>([]);
+    const [loading, setLoading] = useState(true);
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchedInput(event.target.value);
     };
 
+    useEffect(() => {
+        async function getDataFromAPI() {
+            const response = await fetch(
+                "https://api.tvmaze.com/shows/527/episodes"
+            );
+            const jsonBody = await response.json();
+            setFetchedEpisodes(jsonBody);
+            setLoading(false);
+        }
+
+        //reinsuring that this is run only once, on mount.
+        getDataFromAPI();
+    }, []);
+
     const allEpisodes = filterBySearchedInput(
         fetchedEpisodes,
         searchedInput
     ).map((episode) => <EpisodeCard key={episode.id} episode={episode} />);
-
-    // useEffect(() => {
-    //     getDataFromAPI();
-    // }, []);
 
     return (
         <>
@@ -76,10 +66,11 @@ function App() {
                     /{fetchedEpisodes.length}
                 </span>
             </div>
-            {fetchedEpisodes.length === 0 && (
+            {loading ? (
                 <p className="loading">Loading...</p>
+            ) : (
+                <div className="app">{allEpisodes}</div>
             )}
-            <div className="app">{allEpisodes}</div>
             <Footer />
         </>
     );
